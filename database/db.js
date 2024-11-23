31f9b34c71cf6ca99330050f9828ebd0da8d031b
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2024-11-23 14:31:01 +0600

    form fill up server

---
+++ database/db.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdnsrak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function connect() {
+  await client.connect();
+}
+
+function getStudentsData() {
+  return client.db("form-fill-up").collection("studentsData");
+}
+
+function usersCollection() {
+  return client.db("form-fill-up").collection("users");
+}
+
+function studentsData() {
+  return client.db("form-fill-up").collection("studentsData");
+}
+
+module.exports = {
+  connect,
+  getStudentsData,
+  usersCollection,
+  studentsData
+};
---
+++ index.js
@@ -0,0 +1,162 @@
+const express = require("express");
+const db = require("./database/db");
+const { ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const body_parser = require("body-parser");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+// * Middleware:
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(body_parser.json());
+
+// * Connect With mongoDb:
+db.connect();
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "unauthorize access" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorize access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+// * JWT:
+app.post("/jwt", (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+  res.send({ token });
+});
+
+// * Verify admin:
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  const usersCollection = db.usersCollection();
+  const query = { email: email };
+  const user = await usersCollection.findOne(query);
+  if (user?.role !== "admin") {
+    return res.status(403).send({ error: true, message: "forbidden message" });
+  }
+  next();
+};
+
+// * To get all users api:
+app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
+  const usersCollection = db.usersCollection();
+  const result = await usersCollection.find().toArray();
+  res.send(result);
+});
+
+// * CHECK ADMIN OR  NOT:
+app.get("/users/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
+  const email = req.params.email;
+  const usersCollection = db.usersCollection();
+
+  if (req.decoded.email !== email) {
+    return res.send({ admin: false });
+  }
+  const query = { email: email };
+  const user = await usersCollection.findOne(query);
+  const result = { admin: user?.role === "admin" };
+  res.send(result);
+});
+
+// * SAVED USER:
+app.post("/users", async (req, res) => {
+  const user = req.body;
+
+  const query = { email: user.email };
+  const usersCollection = db.usersCollection();
+  const existingUser = await usersCollection.findOne(query);
+  if (existingUser) {
+    return res.send({ message: "User already exist" });
+  }
+
+  const result = await usersCollection.insertOne(user);
+  res.send(result);
+});
+
+// * UPDATE USER ROLE:
+app.put("/users/:id", verifyJWT, verifyAdmin, async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const options = { upsert: true };
+  const updatedRole = req.body;
+  const setNewRole = {
+    $set: {
+      role: updatedRole.role,
+    },
+  };
+  const usersCollection = db.usersCollection();
+  const result = await usersCollection.updateOne(filter, setNewRole, options);
+  res.send(result);
+});
+
+// * Delete User:
+app.delete("/delete-user/:id", verifyJWT, verifyAdmin, async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const usersCollection = db.usersCollection();
+  const result = await usersCollection.deleteOne(query);
+  res.send(result);
+});
+
+// * Get All Students Data:
+// * To get all users api:
+app.get("/allStudents", async (req, res) => {
+  const search = req.query.search;
+  console.log(search);
+  const numericSearch = parseInt(search) || search;
+  const query = {
+    $or: [
+      { phone: { $regex: search, $options: "i" } },
+      { board_roll: { $regex: search, $options: "i" } },
+      { registration: { $regex: search, $options: "i" } },
+      { name: { $regex: search, $options: "i" } },
+      { semester: { $regex: search, $options: "i" } },
+      { shift: { $regex: search, $options: "i" } },
+      { session: { $regex: search, $options: "i" } },
+    ],
+  };
+
+  const studentsData = db.studentsData();
+  const result = await studentsData.find(query).toArray();
+  res.send(result);
+});
+
+// * Add new Students:
+app.post("/addStudent", verifyJWT, verifyAdmin, async (req, res) => {
+  const newStudent = req.body;
+  const studentsData = db.studentsData();
+  const result = await studentsData.insertOne(newStudent);
+  res.send(result);
+});
+
+app.get("/", (req, res) => {
+  res.send("form fil up server is running");
+});
+
+app.listen(port, () => {
+  console.log(`form fil up server is running on port : ${port}`);
+});
